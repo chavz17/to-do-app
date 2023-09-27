26854532c411bc9329bd6ce9ca8ac649535e79f1
chavz17 <gabriel_chavez@dlsu.edu.ph>
2023-09-27 19:02:34 +0800

    add notes feature
    Adds new notes to the list of notes

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-function display() {
-    document.getElementById("h1").innerHTML = "JS file worked!"
+function add() {
+    const input = document.querySelector("input");
+    const addBtn = document.querySelector("#addBtn");
+    const cancelBtn = document.querySelector("#cancelBtn");
+    const div = document.createElement("div");
+    const heading = document.querySelector("h1");
+
+    div.className = "list-item";
+    div.innerHTML = input.value;
+    heading.after(div);
+
+    input.setAttribute("hidden", "hidden");
+    addBtn.setAttribute("hidden", "hidden");
+    cancelBtn.setAttribute("hidden", "hidden");
+}
+
+function cancel() {
+    const input = document.querySelector("input");
+    const addBtn = document.querySelector("#addBtn");
+    const cancelBtn = document.querySelector("#cancelBtn");
+
+    input.setAttribute("hidden", "hidden");
+    addBtn.setAttribute("hidden", "hidden");
+    cancelBtn.setAttribute("hidden", "hidden");
+}
+
+function addItem() {
+    const input = document.querySelector("input");
+    const addBtn = document.querySelector("#addBtn");
+    const cancelBtn = document.querySelector("#cancelBtn");
+
+    input.removeAttribute("hidden");
+    addBtn.removeAttribute("hidden");
+    cancelBtn.removeAttribute("hidden");
 }
